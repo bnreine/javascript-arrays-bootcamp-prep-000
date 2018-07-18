bc0c8bea2678e919a4043db4758fab1225d4c5a6
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-18 14:08:27 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars=['snickers', 'hundred grand', 'kitkat','skittles']
+
+
+function addElementToBeginningOfArray(array,element) {
+  const myArray=array
+  newArray=[element,...myArray]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array.unshift(element)
+  return array
+}
